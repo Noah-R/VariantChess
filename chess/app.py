e7b9b3f674e54887e31479b346b499ae415fbebc
Noah Rutkovsky <noah.rutkovsky@gmail.com>
2024-07-16 19:35:30 -0400

    Staging React frontend and Flask backend

---
+++ chess/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_cors import CORS, cross_origin
+from game import Game
+
+app = Flask(__name__)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+game = Game()
+
+@app.route("/<string:move>")
+@cross_origin()
+def get_position(move = "a1a1x"):
+	x = ord(move[0]) - 97
+	y = ord(move[1]) - 49
+	targetX = ord(move[2]) - 97
+	targetY = ord(move[3]) - 49
+	
+	if(len(move) == 4):
+		game.move(y, x, targetY, targetX, "")
+	else:
+		game.move(y, x, targetY, targetX, move[4])
+	
+	return f'{game.getFEN(piecesOnly = True)[:-1]}'
